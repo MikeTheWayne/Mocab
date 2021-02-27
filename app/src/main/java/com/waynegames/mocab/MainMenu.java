96dfc17782ad7ff05f4b58ae1d0bed6de6e21e8d
py00103 <polya.yaneva@abv.bg>
2021-02-27 16:57:21 +0000

    Add Main Menu Activity

---
+++ app/src/main/java/com/waynegames/mocab/MainMenu.java
@@ -0,0 +1,29 @@
+package com.waynegames.mocab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenu.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
